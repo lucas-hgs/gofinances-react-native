f04a3c3c4235216d904af1b9306fdaaaa73beb73
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2022-01-03 21:45:56 -0300

    Added Resume Listing

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { HistoryCard } from '../../components/HistoryCard';
+import { categories } from '../../utils/categories';
 
 import {
   Container,
   Header,
-  Title
+  Title,
+  Content
 } from './styles';
 
+interface TransactionData {
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+}
+
+interface CategoryData {
+  key: string;
+  name: string;
+  total: string;
+  color: string;
+}
+
 export function Resume() {
+  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+  useEffect(() => {
+    loadData();
+  }, [])
+
   async function loadData() {
     const dataKey = '@gofinances:transactions';
-    const data = await AsyncStorage.getItem(dataKey);
-    const currentData = data ? JSON.parse(data) : [];
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    const expenses = responseFormatted.filter((expense : TransactionData) => expense.type === 'negative');
+
+    const totalByCategory: CategoryData[] = [];
+
+    categories.forEach(category => {
+      let categorySum = 0;
+
+      expenses.forEach((expense : TransactionData) => {
+        if(expense.category === category.key) {
+          categorySum += Number(expense.amount);
+        }
+      });
+
+      if(categorySum > 0) {
+        const total = categorySum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        });
+
+        totalByCategory.push({
+          key: category.key,
+          name: category.name,
+          color: category.color,
+          total,
+        });
+      }
+    });
+
+    setTotalByCategories(totalByCategory);
   }
 
   return (
@@ -21,12 +74,18 @@ export function Resume() {
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
-
-      <HistoryCard 
-        title='Compras'
-        amount='R$150,50'
-        color="red"
-      />
+      
+      <Content>
+        {
+          totalByCategories.map(item => ( 
+            <HistoryCard 
+              title={item.name}
+              amount={item.total}
+              color={item.color}
+            />
+          ))
+        }
+      </Content>
     </Container>
   )
 }
